0881d423d95d0f0c6c4c29c4c420bba6fc437aa9
Larimar4you <Zajanezlaja@gmail.com>
2025-03-15 19:08:33 +0100

    Первый коммит моего проекта-героя 😎

---
+++ src/main.js
@@ -0,0 +1,10 @@
+import './js/covers.js';
+
+import './js/about_me.js';
+import './js/benefits.js';
+import './js/faq.js';
+import './js/header.js';
+import './js/hero.js';
+import './js/projects.js';
+import './js/reviews.js';
+import './js/work_together.js';
