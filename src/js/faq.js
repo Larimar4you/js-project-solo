a87088636f60c2b502c2f26a04a385e73300440c
Larimar4you <Zajanezlaja@gmail.com>
2025-03-17 23:45:50 +0100

    Автокоммит

--- src/js/faq.js
+++ src/js/faq.js
@@ -1,19 +1,19 @@
-import Accordion from 'accordion-js';
-import 'accordion-js/dist/accordion.min.css';
+//  import Accordion from 'accordion-js';
+// import 'accordion-js/dist/accordion.min.css';
 
-export const accord = new Accordion('.container-first', {
-  duration: 500,
-  showMultiple: true,
-  onOpen: function (currentEl) {
-    const btnShowContent = currentEl.querySelector('.icon-btn-show-content');
-    const btnHideContent = currentEl.querySelector('.icon-btn-hide-content');
-    btnHideContent.classList.remove('visually-hidden');
-    btnShowContent.classList.add('visually-hidden');
-  },
-  onClose: function (currentEl) {
-    const btnShowContent = currentEl.querySelector('.icon-btn-show-content');
-    const btnHideContent = currentEl.querySelector('.icon-btn-hide-content');
-    btnHideContent.classList.add('visually-hidden');
-    btnShowContent.classList.remove('visually-hidden');
-  },
-});
+// export const accord = new Accordion('.container-first', {
+//   duration: 500,
+//   showMultiple: true,
+//   onOpen: function (currentEl) {
+//     const btnShowContent = currentEl.querySelector('.icon-btn-show-content');
+//     const btnHideContent = currentEl.querySelector('.icon-btn-hide-content');
+//     btnHideContent.classList.remove('visually-hidden');
+//     btnShowContent.classList.add('visually-hidden');
+//   },
+//   onClose: function (currentEl) {
+//     const btnShowContent = currentEl.querySelector('.icon-btn-show-content');
+//     const btnHideContent = currentEl.querySelector('.icon-btn-hide-content');
+//     btnHideContent.classList.add('visually-hidden');
+//     btnShowContent.classList.remove('visually-hidden');
+//   },
+// });
